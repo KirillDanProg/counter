27823853cd0aa733041390b0f612a8250d2a0403
KirillDanProg <kirill2898v@gmail.com>
2022-08-15 16:34:11 +0300

    update: maxValue startValue added to props

---
+++ src/components/Counter/CounterBox.tsx
@@ -0,0 +1,35 @@
+import styles from "../Counter.module.css";
+import React, {FC} from "react";
+import {CounterControllers} from "./CounterControllers";
+import {CounterTotal} from "./CounterTotal";
+
+
+type CounterBoxType = {
+    count: number
+    setCount: (n: number) => void
+    resetCount: () => void
+    maxValue: number
+    startValue: number
+}
+
+export const CounterBox: FC<CounterBoxType> = ({count, setCount, resetCount, maxValue, startValue}) => {
+    return (
+        <div className={`${styles.innerContainer}  ${count >= maxValue ? styles.contHit : ""}`}>
+            <div className={styles.mainContent}>
+                <CounterTotal count={count}
+                              maxValue={maxValue}
+                />
+            </div>
+
+            <div className={styles.secondaryContent}>
+                <CounterControllers count={count}
+                                    setCount={setCount}
+                                    resetCount={resetCount}
+                                    maxValue={maxValue}
+                                    startValue={startValue}
+                />
+            </div>
+        </div>
+    )
+}
+
