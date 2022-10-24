8df08e96c0127b59772f36ac0331a0a9b062d042
KirillDanProg <kirill2898v@gmail.com>
2022-10-24 12:02:39 +0300

    uploading from localStorage added

--- src/components/Counter/CounterBox.tsx
+++ src/components/Counter/CounterBox.tsx
@@ -1,17 +1,30 @@
 import styles from "../Counter.module.css";
-import React from "react";
+import React, {useEffect} from "react";
 import {CounterControllers} from "./CounterControllers";
 import {CounterTotal} from "./CounterTotal";
 import {useSelector} from "react-redux";
 import {RootState} from "../../store/store";
+import {useAppDispatch} from "../../hooks/hooks";
+import {getCountTC, getLocalStorageValuesTC, setCountTC} from "../../store/reducer/counterReducer";
 
 export const CounterBox = React.memo(() => {
-
     const minValue = useSelector<RootState, number>(state => state.counter.minValue)
     const maxValue = useSelector<RootState, number>(state => state.counter.maxValue)
     const count = useSelector<RootState, number>(state => state.counter.count)
 
-    console.log("CounterBox")
+    const dispatch = useAppDispatch()
+
+    useEffect(() => {
+        dispatch(getLocalStorageValuesTC())
+    }, [])
+
+    useEffect(() => {
+        dispatch(getCountTC())
+    }, [])
+
+    useEffect(() => {
+        dispatch(setCountTC())
+    }, [count])
 
     return (
         <div className={`${styles.innerContainer}  ${count >= maxValue ? styles.contHit : ""}`}>
